b99f9164ae57824cea33b526b2472637b1f7e8e9
harrj396 <ahletmethink@gmail.com>
2022-12-12 06:41:01 -0500

    Added product belongs to many

--- models/index.js
+++ models/index.js
@@ -12,12 +12,13 @@ Product.belongsTo(Category, {
 Category.hasMany(Product,{
   foreignKey: "category_id",
 });
-
-
-
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 
 // Tags belongToMany Products (through ProductTag)
+
 
 module.exports = {
   Product,
